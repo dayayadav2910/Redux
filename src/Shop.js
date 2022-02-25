b610a322211165d23c9484e578489306881910d2
Daya Yadav <dayay@Dayas-MacBook-Air.local>
2022-02-25 11:49:03 +0530

    message is added

--- src/Shop.js
+++ src/Shop.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { actionCreators } from './state/index'
 
@@ -7,11 +7,11 @@ import { actionCreators } from './state/index'
 function Shop() {
     const dispatch = useDispatch();
     const {withdrawmoney,depositemoney} = bindActionCreators(actionCreators,dispatch);
-    
+    const balance = useSelector(state => state.amount)
   return (
     <div className='container'>
     <button className="btn btn-primary my-3 mx-3" onClick={()=>{withdrawmoney(100)}}>-</button>
-    Add item to the cart
+    Update Balance ({balance})
     <button className="btn btn-primary my-3 mx-3" onClick={()=>{depositemoney(100)}}>+</button>
     </div>
   )
